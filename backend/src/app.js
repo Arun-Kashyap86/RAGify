7c6f5feec27ba37971545eff0cf1b03c3a149226
Arun Kashyap <arunkkashyap86@gmail.com>
2026-09-15 16:38:47 +0530

    cors updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://ra-gify-eta.vercel.app/", "http://localhost:5173"],
+    origin: ["https://ra-gify-eta.vercel.app", "http://localhost:5173"],
     credentials: true,
   }),
 );
