4efcc7c65c2b26bbbef17fdb8b0e8febe6c4c87d
Arun Kashyap <arunkkashyap86@gmail.com>
2026-09-15 15:52:36 +0530

    with cloud api link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
     import.meta.env.VITE_API_BASE_URL ||
-    "http://localhost:5000/api",
+    "https://ragify-0usx.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
